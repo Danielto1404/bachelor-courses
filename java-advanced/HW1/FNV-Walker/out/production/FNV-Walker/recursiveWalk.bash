b62651c8392a2f20e4c73ffc3fed32299ab93068
Danielto1404 <daniilkorolev00@gmail.com>
2020-02-19 19:59:33 +0300

    JAVA ADVANCED

--- java-advanced/HW1/FNV-Walker/out/production/FNV-Walker/recursiveWalk.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp . -p . -m info.kgeorgiy.java.advanced.walk RecursiveWalk ru.ifmo.rain.korolev.walk.RecursiveWalker M3235
--- java-advanced/HW1/FNV-Walker/out/production/FNV-Walker/walk.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp . -p . -m info.kgeorgiy.java.advanced.walk Walk ru.ifmo.rain.korolev.walk.Walker M3235
