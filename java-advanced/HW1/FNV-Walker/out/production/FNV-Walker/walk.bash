fcd253310d0b0062a3ba0e4ba90e93e3ec38429c
Danielto1404 <daniilkorolev00@gmail.com>
2020-02-19 19:31:01 +0300

    FIRST HW

--- java-advanced/HW1/FNV-Walker/out/production/FNV-Walker/walk.bash
+++ java-advanced/HW1/FNV-Walker/out/production/FNV-Walker/walk.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp . -p . -m info.kgeorgiy.java.advanced.walk Walk ru.ifmo.rain.korolev.walk.Walker
+java -cp . -p . -m info.kgeorgiy.java.advanced.walk Walk ru.ifmo.rain.korolev.walk.Walker M3235
