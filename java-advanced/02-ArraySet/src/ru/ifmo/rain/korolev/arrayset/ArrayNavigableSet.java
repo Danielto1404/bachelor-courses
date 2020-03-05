51b1c40523546e23d619801295c95d3b9d584615
Danielto1404 <daniilkorolev00@gmail.com>
2020-03-05 14:28:59 +0300

    HARD

--- java-advanced/02-ArraySet/src/ru/ifmo/rain/korolev/arrayset/ArrayNavigableSet.java
+++ java-advanced/02-ArraySet/src/ru/ifmo/rain/korolev/arrayset/ArrayNavigableSet.java
@@ -203,37 +203,12 @@ public class ArrayNavigableSet<E> extends AbstractSet<E> implements NavigableSet
      * Unsupported operations for unmodifiable collection
      */
     @Override
-    public boolean addAll(Collection<? extends E> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
     public E pollFirst() {
         throw new UnsupportedOperationException();
     }
 
     @Override
     public E pollLast() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
         throw new UnsupportedOperationException();
     }
 }
