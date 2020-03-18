1b44d32dd29ca75366a32682fcecf923989e8cf4
Danielto1404 <daniilkorolev00@gmail.com>
2020-03-19 01:07:00 +0300

    EASY

--- java-advanced/07-iterative-parallelism/src/ru/ifmo/rain/korolev/concurrent/IterativeParallelism.java
+++ java-advanced/07-iterative-parallelism/src/ru/ifmo/rain/korolev/concurrent/IterativeParallelism.java
@@ -21,75 +21,69 @@ public class IterativeParallelism implements ScalarIP {
     /**
      * Creates an almost uniform distribution of tasks performed on a given number of threads {@link Thread}
      *
-     * @param threads Number of threads to use.
-     * @param tasks   List {@link List} of tasks to be completed.
      * @param <T>     Associated type for task.
+     * @param threads Number of threads to use.
+     * @param values  List {@link List} of tasks to be completed.
      * @return {@link List} returns list of tasks distributed on given number of threads {@link Thread}
      */
-    private <T> List<List<T>> makeDistributionForThreads(int threads, List<T> tasks) {
+    private <T> List<Stream<T>> makeDistributionForThreads(int threads, List<T> values) {
+
         if (threads <= 0) {
-            throw new IllegalArgumentException("Threads quantity must be greater than 0");
+            throw new IllegalArgumentException("Number of threads must be positive");
         }
-
-        threads = Math.max(threads, Math.min(tasks.size(), threads));
-        int taskSize = tasks.size() / threads;
-        int remainder = tasks.size() % threads;
-        List<List<T>> distribution = new ArrayList<>();
-
-        int taskIndex = 0;
-        for (int thread = 0; thread < threads; thread++) {
-            int currentTaskSize = taskSize + (thread < remainder ? 1 : 0);
-            if (currentTaskSize > 0) {
-                distribution.add(tasks.subList(taskIndex, taskIndex + currentTaskSize));
-            }
-            taskIndex += currentTaskSize;
+        threads = Math.max(1, Math.min(threads, values.size()));
+        final List<Stream<T>> distribution = new ArrayList<>();
+        final int groupSize = values.size() / threads;
+        int extraTasks = values.size() % threads;
+        for (int i = 0, end = 0; i < threads; i++) {
+            int begin = end;
+            end = begin + groupSize + (extraTasks > 0 ? 1 : 0);
+            extraTasks--;
+            distribution.add(values.subList(begin, end).stream());
         }
 
         return distribution;
     }
 
-    private <T, B, R> R runIP(int threads, List<T> list, Function<Stream<T>, B> batchJob,
-                              Function<Stream<B>, R> reduceFunction) throws InterruptedException {
-        List<List<T>> tasksDistributions = makeDistributionForThreads(threads, list);
-        threads = tasksDistributions.size();
-        List<B> tasksResults = new ArrayList<>(Collections.nCopies(threads, null));
-        List<Thread> workers = new ArrayList<>();
+    /**
+     * @param threads {@link List} of threads to join.
+     * @throws InterruptedException throws in case some thread didn't join.
+     */
+    private static void joinTasks(final List<Thread> threads) throws InterruptedException {
+        boolean isInterrupted = false;
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                isInterrupted = true;
+            }
+        }
+        if (isInterrupted) {
+            throw new InterruptedException("Some thead didn't join");
+        }
+    }
 
+    private <T, R> R baseTask(int threads, List<T> list, Function<Stream<T>, R> tasksFunction,
+                              Function<Stream<R>, R> collector) throws InterruptedException {
+        List<Stream<T>> tasksDistributions = makeDistributionForThreads(threads, list);
+        threads = tasksDistributions.size();
+        List<R> tasksResults = new ArrayList<>(Collections.nCopies(threads, null));
+        List<Thread> workers = new ArrayList<>();
         for (int index = 0; index < threads; index++) {
-            final int finalIndex = index;
+            final int thisIndex = index;
             Thread thread = new Thread(
-                    () -> tasksResults.set(finalIndex, batchJob.apply(tasksDistributions.get(finalIndex).stream())));
+                    () -> tasksResults.set(thisIndex, tasksFunction.apply(tasksDistributions.get(thisIndex))));
             workers.add(thread);
             thread.start();
         }
-
-        for (int i = 0; i < threads; i++) {
-            try {
-                workers.get(i).join();
-            } catch (InterruptedException e) {
-                InterruptedException exception = new InterruptedException("Some threads were interrupted");
-                exception.addSuppressed(e);
-                for (int j = i; j < threads; j++) {
-                    workers.get(j).interrupt();
-                }
-                for (int j = i; j < threads; j++) {
-                    try {
-                        workers.get(j).join();
-                    } catch (InterruptedException e1) {
-                        e.addSuppressed(e1);
-                        --j;
-                    }
-                }
-                throw exception;
-            }
-        }
-        return reduceFunction.apply(tasksResults.stream());
+        joinTasks(workers);
+        return collector.apply(tasksResults.stream());
     }
 
     @Override
     public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator)
             throws InterruptedException {
-        return runIP(threads, values,
+        return baseTask(threads, values,
                 stream -> stream.max(comparator).orElseThrow(),
                 stream -> stream.max(comparator).orElseThrow());
     }
@@ -103,7 +97,7 @@ public class IterativeParallelism implements ScalarIP {
     @Override
     public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate)
             throws InterruptedException {
-        return runIP(threads, values,
+        return baseTask(threads, values,
                 stream -> stream.allMatch(predicate),
                 stream -> stream.allMatch(Boolean::booleanValue));
     }
