87069685665662f115d4ad2000e22de86f1765d7
Danielto1404 <daniilkorolev00@gmail.com>
2021-02-11 15:56:28 +0300

    remove 4-task

--- mathematical-logic/04-completeness-of-calculus/Parsing/ProofParser.hs
+++
@@ -1,8 +0,0 @@
-module Parsing.ProofParser  where
-
-import Parsing.Lexer
-import Parsing.Grammar
-import Logic.Expression (Expression(..))
-
-parseExpression :: String -> Expression
-parseExpression = parse . alexScanTokens
